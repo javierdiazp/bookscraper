a3d10b53daed73a2b30d49917f7da1da5198a77c
Javier <javier.diaz.palacios@gmail.com>
2019-10-09 21:28:14 -0300

    Se crea modelo para API con Category y Book

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    thumbnail_url = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    stock = models.BooleanField(default=False)
+    product_description = models.CharField(max_length=200)
+    upc = models.CharField(max_length=30)

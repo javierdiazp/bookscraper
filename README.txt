ae0f7685925523f0cdde295ba9735a60e0edf4bc
Javier <javier.diaz.palacios@gmail.com>
2019-10-10 15:16:46 -0300

    Obtencion de detalle de libros. Faltan manejo de excepciones para tags no encontrados.

---
+++ README.txt
@@ -0,0 +1,8 @@
+INSTRUCCIONES DE USO
+
+1. SCRAPING
+El proceso de scraping se realiza bajo la interfaz manage.py de Django.
+Para invocarla se debe ejecutar el comando 'python manage.py scrapebooks.py'
+
+2. API
+Pendiente...
--- api/management/commands/scrapebooks.py
+++ api/management/commands/scrapebooks.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
-from api.models import Category
+from api.models import Category, Book
 
 import requests
 from requests.exceptions import HTTPError
 from requests.compat import urljoin
 from bs4 import BeautifulSoup
+
+import re
 
 
 class Command(BaseCommand):
@@ -13,57 +15,85 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         try:
-            categories = self.get_categories()
+            categories = get_categories(self.path)
             for category in categories:
-                c = Category(name=category['name'])
-                # c.save()
+                c_name, c_url = category
+                c = Category(name=c_name)
+                c.save()
 
-                category_url = self.path + category['href']
-                while category_url:
-                    htmlparsed = self.get_html_parsed(category_url)
-                    books = self.get_books(htmlparsed)
-                    category_url = self.get_next_page(category_url, htmlparsed)
+                while c_url:
+                    # print(c_url)
+                    htmlparsed = get_html_parsed(c_url)
+                    books_url = get_books_url(c_url, htmlparsed)
+                    c_url = get_next_page(c_url, htmlparsed)
+
+                    for b_url in books_url:
+                        book = get_book_detail(b_url)
+                        b = Book(category_id=c, **book)
+                        b.save()
 
         except HTTPError as http_err:
-            raise CommandError(f'Error en la peticion HTTP: {http_err}')
+            raise CommandError(f'Http Request Error: {http_err}')
         except Exception as err:
             raise CommandError(f'Error: {err}')
         else:
             self.stdout.write(self.style.SUCCESS('Scraping finalizado correctamente'))
 
-    def get_categories(self):
-        response = requests.get(self.path)
-        response.raise_for_status()
 
-        soup = BeautifulSoup(response.text, 'html.parser')
-        tags = soup.find(class_='side_categories').find_all('a')
-        tags.pop(0)
+def get_html_parsed(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return BeautifulSoup(response.text, 'html.parser')
 
-        categories = []
 
-        for tag in tags:
-            categories.append({'name': tag.string.strip(), 'href': tag.get('href')})
+def get_categories(url):
+    soup = get_html_parsed(url)
+    category_tags = soup.find(class_='side_categories').find_all('a')
+    category_tags.pop(0)
 
-        return categories
+    categories = []
 
-    @staticmethod
-    def get_html_parsed(url):
-        response = requests.get(url)
-        response.raise_for_status()
-        return BeautifulSoup(response.text, 'html.parser')
+    for tag in category_tags:
+        categories.append((tag.string.strip(), urljoin(url, tag.get('href'))))
 
-    @staticmethod
-    def get_books(htmlparsed):
-        books_uri = []
-        for article in htmlparsed.find_all('article', class_='product_pod'):
-            books_uri.append(article.find('a').get('href'))
-        return books_uri
+    return categories
 
-    @staticmethod
-    def get_next_page(baseurl, htmlparsed):
-        nextpage = htmlparsed.find('li', class_='next')
-        if nextpage is None:
-            return ""
-        else:
-            href = nextpage.find('a').get('href')
-            return urljoin(baseurl, href)
+
+def get_books_url(baseurl, htmlparsed):
+    books_url = []
+    for article in htmlparsed.find_all('article', class_='product_pod'):
+        href = article.find('a').get('href')
+        books_url.append(urljoin(baseurl, href))
+    return books_url
+
+
+def get_next_page(baseurl, htmlparsed):
+    nextpage = htmlparsed.find('li', class_='next')
+    if nextpage is None:
+        return ""
+    else:
+        href = nextpage.find('a').get('href')
+        return urljoin(baseurl, href)
+
+
+def get_book_detail(url):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    title = soup.find(class_='product_main').find('h1')
+    thumbnail = soup.find(class_='thumbnail').find('img')
+    price = title.find_next_sibling('p')
+    stock = price.find_next_sibling('p').find('i', class_='icon-ok')
+    description = soup.find(id='product_description').find_next_sibling('p')
+    upc = soup.find('th', string='UPC').find_next_sibling('td')
+
+    return {
+        'title': title.string,
+        'thumbnail_url': thumbnail.get('src'),
+        'price': float(re.findall("\d+\.\d+", price.string)[0]),
+        'stock': stock is not None,
+        'product_description': description.string,
+        'upc': upc.string
+    }
